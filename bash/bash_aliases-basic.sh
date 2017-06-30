7852c0c6f4ea2814de08d34d26af51f2ca351eff
Eplistical <gaohan@sas.upenn.edu>
2017-06-30 14:01:27 -0400

    save

--- bash/bash_aliases-basic.sh
+++ bash/bash_aliases-basic.sh
@@ -9,7 +9,7 @@ alias bra="vi $HOME/.bash_aliases"
 alias brc="vi $HOME/.bashrc"
 alias vrc="vi $HOME/.vimrc"
 alias sb="source $HOME/.bashrc"
-alias ls="ls --color=auto --sort=extension"
+alias ls="ls --color=always --group-directories-first --sort=extension"
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
