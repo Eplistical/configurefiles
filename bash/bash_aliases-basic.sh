02a4bb7d3e72a390474cd507d8bf0d188878dcba
Gaohan Miao <gaohan@sas.upenn.edu>
2017-06-30 13:49:40 -0400

    modified bash/

--- bash/bash_aliases-basic.sh
+++ bash/bash_aliases-basic.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-alias rm="mv2recycle"
+alias rm='mv2recycle'
+alias sshy='ssh -Y'
 alias chrome='google-chrome'
 alias Foxit='FoxitReader'
+alias vi='vim'
 alias bra="vi $HOME/.bash_aliases"
 alias brc="vi $HOME/.bashrc"
 alias vrc="vi $HOME/.vimrc"
 alias sb="source $HOME/.bashrc"
-alias ls="ls --color=auto --group-directories-first --sort=extension"
+alias ls="ls --color=auto --sort=extension"
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias e='emacs -nw'
+alias ll='ls -l'
